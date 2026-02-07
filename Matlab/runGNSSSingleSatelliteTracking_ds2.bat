2b6b5db2e2ea7c3f2634d1b487a84ced5616322a
juliansimon <julian.simon@gmail.com>
2026-02-07 13:29:49 +0100

    Add files matlab

---
+++ Matlab/runGNSSSingleSatelliteTracking_ds2.bat
@@ -0,0 +1,3 @@
+"D:\Programas\MATLAB\bin\matlab" -nosplash -nodesktop -minimize -r "addpath(genpath('D:\TFG-Hugo\GNSSGPS-Spoofing\Matlab\FGI-GSRx')); gsrx('D:\TFG-Hugo\GNSSGPS-Spoofing\Matlab\complete_Texbat_Dataset-ds2_GPS_L1_CA.txt')"
+
+                                                                                     
